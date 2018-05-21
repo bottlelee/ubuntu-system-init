30a12c1b7c92c7e33c4b78071b55a5379382c702
Haibin Lee <haibin.li@newtouch.cn>
2018-05-21 20:09:13 +0800

    WIP

--- init_ansible.sh
+++ init_ansible.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt-get install -y python-pip git
+sudo apt-get install -y python-pip git python-apt
 
 sudo -H pip install -i https://pypi.doubanio.com/simple -U pip
 sudo -H pip install -i https://pypi.doubanio.com/simple ansible
